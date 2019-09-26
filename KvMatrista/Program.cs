570a8c2a93a788e0c450bef305679b63d99936f6
rvpermyakov <permyakov.rooma@gmail.com>
2019-09-27 03:47:31 +0500

    Task Five

---
+++ KvMatrista/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvMatrista
+{
+    class Program
+    {
+        static void Main()
+        {
+            var matr = new int[8, 8];
+            var k =1;
+            Console.WriteLine("Сортированный массив");
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(0); j++)
+                {
+                    matr[i, matr.GetLength(0) - j - 1 + (2 * j + 1 - matr.GetLength(0)) * (i % 2)] = k++;
+                }
+            }
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(0); j++)
+                    Console.Write(matr[j, i] + "\t");
+                Console.WriteLine();
+            }
+        }
+    }
+}
